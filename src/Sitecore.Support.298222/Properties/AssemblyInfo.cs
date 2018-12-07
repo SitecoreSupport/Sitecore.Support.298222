337d7c9387c3d39a8d4fc7bdab9ead3f03b33cb9
Calvin Williams <40271790+sc-CalvinWilliams@users.noreply.github.com>
2018-12-07 15:07:03 -0500

    init

---
+++ src/Sitecore.Support.298222/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.298222")]
+[assembly: AssemblyProduct("Sitecore.Support.298222")]
+[assembly: ComVisible(false)]
